320cf6402922785963f3f91b855d65a227a3e05d
moriyama yuki <moriyama0206@gmail.com>
2012-07-26 00:21:50 +0900

    aa

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Atk
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
