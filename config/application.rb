32c28d97e4bf8a5ad9a7b780b6e32227aef67c8c
moriyama yuki <moriyama0206@gmail.com>
2012-07-27 13:27:48 +0900

    aa

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Atk
     config.assets.compile = true
     config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
